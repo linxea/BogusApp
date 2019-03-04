29d50f04e3e41a69fc6b61c6d5fe765d9b78ecdf
linxea <linxea.linxea@gmail.com>
2019-03-05 00:05:32 +0800

    Now i know why it is so much better to use express

--- server.js
+++ server.js
@@ -1,64 +1,83 @@
-const http = require("http");
-var fs = require("fs");
+const express = require("express");
 const WebSocket = require("ws");
+const uuidv4 = require("uuid/v4");
+const http = require("http");
 
 const port = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
 
-const requestHandler = (request, response) => {
-  const url = request.url;
+// The URLs that we care
+app.get("/", (req, res) =>
+  res.send(
+    "Hello you are on the server, go to <a href='https://bogusapp.herokuapp.com/web'>web</a> or <a href='https://bogusapp.herokuapp.com/mobile'>mobile</a>"
+  )
+);
+app.get("/web", (req, res) => res.sendFile(__dirname + "/web.html"));
+app.get("/mobile", (req, res) => res.sendFile(__dirname + "/mobile.html"));
+app.use("/static", express.static("public"));
 
-  if (url == "/web") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    var webHtml = fs.readFileSync("web.html");
-    response.end(webHtml);
-  } else if (url == "/mobile") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    var mobileHtml = fs.readFileSync("mobile.html");
-    response.end(mobileHtml);
-  } else {
-    response.end("Hello you are on the server, go to /web or /mobile");
-  }
-};
+// Everything else go to 404
+app.get("*", (req, res) => res.sendFile(__dirname + "/404.html"));
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, err => {
-  if (err) {
-    return console.log("Something went wrong.", err);
-  }
-
-  console.log(`Server is listening on ${port}`);
-});
+const queueOfConnections = {};
+const validQRCodes = [];
 
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", ws => {
+  // Create a unique id for each websocket connection and store each connection in a queue
+  const id = uuidv4();
+  validQRCodes.push(id);
+  queueOfConnections[id] = ws;
+
   ws.on("message", response => {
     const payload = JSON.parse(response);
     const { action } = payload;
 
     switch (action) {
       case "get_qr_code":
-        payload.qrCode = "12345";
+        payload.qrCode = id;
         ws.send(JSON.stringify(payload));
         console.log(`Action: ${action}, QR Code: ${payload.qrCode}`);
         break;
       case "validate_qr_code":
         const { qrCode, username } = payload;
-        if (qrCode === "12345") {
+        console.log(
+          "valid",
+          validQRCodes,
+          qrCode,
+          validQRCodes.includes(qrCode)
+        );
+
+        if (validQRCodes.includes(qrCode)) {
           payload.message = "Login successfully!";
           payload.action = "validated_qr_code";
-          ws.send(JSON.stringify(payload));
+
+          const validatePayload = JSON.stringify(payload);
+
+          // Once QR Code is validated, update both mobile and web connections,
+          // remove id from queueOfConnections and validQRCode and close websocket connection
+          ws.send(validatePayload);
+          queueOfConnections[qrCode].send(validatePayload);
+          delete validQRCodes[validQRCodes.indexOf(qrCode)];
+          delete queueOfConnections[qrCode];
+
           console.log(`Username: ${username}, QR Code: ${qrCode}`);
         } else {
-          payload.message = "Login Error!";
           payload.action = "error";
+          payload.message =
+            "Aiya, there's a problem with your QR code! Scan again!";
           ws.send(JSON.stringify(payload));
+
           console.log(`QR Code is invalid, QR Code : ${qrCode}`);
         }
         break;
       default:
-        ws.send("Invalid action");
+        payload.action = "invalid_action";
+        ws.send(JSON.stringify(action));
+        console.log(`Invalid action, QR Code : ${qrCode}`);
     }
   });
 });
